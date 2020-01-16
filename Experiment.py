49146a52ff769106f14b2030a7090b94a3fbc011
Orange <noah.m.mayer@gmail.com>
2020-01-16 14:58:31 +0100

    Update

--- Experiment.py
+++ Experiment.py
@@ -1,3 +1,22 @@
 import numpy as np
 
-a = np.array([], [])
+
+
+
+
+def player_0():
+    pass
+
+
+def player_1():
+    pass
+
+
+a = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' '])
+
+
+player_0()
+
+for game_round in range(0, 4):
+    player_1()
+    player_0()
